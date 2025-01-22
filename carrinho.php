d1fc3b5be1f0b5c9e8086ecd9148930c14885ba8
Felipe ツ <fellipesilva157@hotmail.com>
2025-01-22 14:20:07 -0300

    Create carrinho.php

---
+++ carrinho.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Carrinho</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <header>
+        <nav>
+            <a href="produtos.html">Continuar Comprando</a>
+        </nav>
+    </header>
+    <main>
+        <h1>Seu Carrinho</h1>
+        <p>Em desenvolvimento</p>
+    </main>
+</body>
+</html>
