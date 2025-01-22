cd0af56b327baf78a5e29b58c4452c0827f67e73
Felipe ツ <antonio.pereira312@aluno.ce.gov.br>
2025-01-22 12:48:29 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,6 +2,15 @@ let cart = [];
 
 function addToCart(productName, price) {
     cart.push({ productName, price });
-    alert(`${productName} foi adicionado ao carrinho!`);
+    showNotification(`${productName} foi adicionado ao carrinho!`);
     console.log("Carrinho:", cart);
+}
+
+function showNotification(message) {
+    const notification = document.getElementById('cart-notification');
+    notification.textContent = message;
+    notification.classList.add('show');
+    setTimeout(() => {
+        notification.classList.remove('show');
+    }, 3000);
 }
