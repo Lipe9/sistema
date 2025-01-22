d9ebeb191c32ca8d9f811e3720b3cfcb64921ec1
Felipe ツ <fellipesilva157@hotmail.com>
2025-01-22 15:06:27 -0300

    Update produto.php

--- produto.php
+++ produto.php
@@ -30,7 +30,7 @@ $produto = $produtos[$id];
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo $produto['nome']; ?></title>
-    <link rel="stylesheet" href="assets/css/styles.css">
+    <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
     <header>
