722167c8fa3594863a5b7ef5dc2d4326bd1ee3de
Felipe ツ <fellipesilva157@hotmail.com>
2025-01-22 14:37:52 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
+// Seleciona os elementos do menu e do menu hambúrguer
 const menuHamburguer = document.querySelector(".menu-hamburguer");
 const menu = document.querySelector(".menu");
 
+// Adiciona o evento de clique
 menuHamburguer.addEventListener("click", () => {
-    menu.classList.toggle("open");
+    menu.classList.toggle("open"); // Abre/fecha o menu
+    menuHamburguer.classList.toggle("open"); // Anima o hambúrguer para "X"
 });
